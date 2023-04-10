fea28b2ac5fc8dd61a381749cbc46c4a45ea84e2
Naing-Bhone-Htut <naingphonehtut108@gmail.com>
2023-04-10 12:54:45 +0630

    Card Testing

--- projects/trip-planning/src/app/modules/user/booking/booking.component.ts
+++ projects/trip-planning/src/app/modules/user/booking/booking.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './booking.component.html',
-  styles: [
-  ]
+  styleUrls: ['./booking.component.css']
 })
 export class BookingComponent {
 
